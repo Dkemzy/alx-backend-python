e22cbab91862ec7c0cfd43be886a6f3aa2e8c232
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-04-06 21:50:13 +0300

    python_async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""
+0. Async Generator"""
 import asyncio
+from typing import Generator
 import random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds.
-    '''
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
+async def async_generator() -> Generator[float, None, None]:
+    """The coroutine will loop 10 times, each time
+    asynchronously wait 1 second, then yield a random
+    number between 0 and 10. Use the random module."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
